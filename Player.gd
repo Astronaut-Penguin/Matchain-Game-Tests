22457a6e23158b977e8396cfb7ea6ff1f072302a
Leonel Salvo <yupisenpai@hotmail.com>
2022-06-21 13:22:38 -0300

    Main, player and dialogic

---
+++ Player.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+export var speed = 400
+var screen_size # size of the game window
+signal contact
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport().size
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$AnimatedSprite.play()
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	# If the velocity its a value, then walk |& flip
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment.
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		#The idle animation will be displayed when no movement its detected
+		$AnimatedSprite.animation = "Idle"
+	
+	# Clamp the player position to the screen
+	position += velocity * delta
+	position.x = clamp(position.x,0,screen_size.x)
+	position.y = clamp(position.y,0,screen_size.y)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_Player_body_entered(body):
+	emit_signal("contact")
+	$CollisionShape2D.set_deferred("disabled", true)
